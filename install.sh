227f301396d151439fb654df55ee30e22f1748ee
dsarmien <dsarmien@student.42malaga.com>
2024-09-12 11:39:24 +0200

    Added multiple checks and better code understand

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ then
         echo -e "${COLOR_WHITE}Cuando finalice la intalación, ejecuta de nuevo este script.${COLOR_RESET}"
         exit 1
     else:
-        print_warning "${COLOR_WHITE}No tienes permisos sudo. Por favor contacta con el administrador del sistema...${COLOR_RESET}"
+        print_warning "${COLOR_WHITE}No tienes permisos ${COLOR_YELLOW}sudo${COLOR_WHITE}. Por favor contacta con el administrador del sistema...${COLOR_RESET}"
         print_error
         print_info "${COLOR_WHITE}Utiliza: ${COLOR_CYAN}apt-get upgrade && apt-get install wget${COLOR_RESET}"
         echo -e "${COLOR_WHITE}Cuando finalice la intalación, ejecuta de nuevo este script.${COLOR_RESET}"
@@ -96,18 +96,43 @@ then
 else
     # muestra un mensaje de inicio
     print_info "${COLOR_WHITE}Iniciando la instalación...${COLOR_RESET}"
+    sleep 1
     # crea el directorio ".vim" en tu home, si no existe...
     # también crea el subdirectorio "plugin"
-    mkdir -p ~/.vim/plugin
-    # entra en el subdirectorio plugin anteriormente creado
-    cd ~/.vim/plugin
-    print_ok
-    # descarga el plugin necesario para añadir el header a vim desde el repositorio de github oficial
-    wget -q https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim
-    print_info "${COLOR_WHITE}Descargando el plugin '${COLOR_YELLOW}42header${COLOR_RESET}'..."
-    print_ok
+    VIM_PLUGINS_PATH=$HOME/.vim/plugin
+    if [ ! -d $VIM_PLUGINS_PATH ]; then
+        mkdir -p $VIM_PLUGINS_PATH
+        # entra en el subdirectorio plugin anteriormente creado
+        cd $VIM_PLUGINS_PATH
+        print_ok
+        if [ ! -f $VIM_PLUGINS_PATH/stdheader.vim ]; then
+            # descarga el plugin necesario para añadir el header a vim desde el repositorio de github oficial
+            wget -q https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim
+            print_info "${COLOR_WHITE}Descargando el plugin '${COLOR_YELLOW}42header${COLOR_RESET}'..."
+            print_ok
+        else
+            print_installed "${COLOR_WHITE}El plugin '${COLOR_YELLOW}42header${COLOR_WHITE}' ya está instalado...${COLOR_RESET}"
+            print_pass
+            exit 1
+        fi
+    else
+        print_pass
+        print_installed "${COLOR_WHITE}El directorio '${COLOR_YELLOW}.vim/plugin${COLOR_WHITE}' ya existe...${COLOR_RESET}"
+        print_pass
+        cd $VIM_PLUGINS_PATH
+        if [ ! -f $VIM_PLUGINS_PATH/stdheader.vim ]; then
+            # descarga el plugin necesario para añadir el header a vim desde el repositorio de github oficial
+            wget -q https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim
+            print_info "${COLOR_WHITE}Descargando el plugin '${COLOR_YELLOW}42header${COLOR_RESET}'..."
+            print_ok
+        else
+            print_installed "${COLOR_WHITE}El plugin '${COLOR_YELLOW}42header${COLOR_WHITE}' ya está instalado...${COLOR_RESET}"
+            print_pass
+            exit 1
+        fi
+    fi
     # vuelve a la raíz principal, el home del usuario
-    cd ~
+    cd $HOME
     # se comprueba si se le pasó el nombre de usuario como argumento al script
     if [ -z $1 ]
     then
@@ -116,19 +141,33 @@ else
         print_pass
         print_info "Estableciendo el usuario al del sistema actual '${COLOR_YELLOW}$(whoami)${COLOR_WHITE}'..."
         # se añade al final del archivo las variables user42 y mail42
-        echo "let g:user42 = '$(whoami)'" >> ~/.vimrc
-        echo "let g:mail42 = '$(whoami)@student.42malaga.com'" >> ~/.vimrc
-        echo "USER=$(whoami)" >> ~/.zshrc
-        echo "MAIL=$(whoami)@student.42malaga.com" >> ~/.zshrc
+        if [ ! -f $HOME/.vimrc ]; then
+            touch $HOME/.vimrc
+            echo "let g:user42 = '$(whoami)'" >> $HOME/.vimrc
+            echo "let g:mail42 = '$(whoami)@student.42malaga.com'" >> $HOME/.vimrc
+        else
+            # TODO: Añadir comprobación de variables existentes para evitar duplicados
+            echo "let g:user42 = '$(whoami)'" >> $HOME/.vimrc
+            echo "let g:mail42 = '$(whoami)@student.42malaga.com'" >> $HOME/.vimrc
+        fi
+        if [ ! -f $HOME/.zshrc ]; then
+            touch $HOME/.zshrc
+            echo "export USER=$(whoami)" >> $HOME/.zshrc
+            echo "export MAIL=$(whoami)@student.42malaga.com" >> $HOME/.zshrc
+        else
+            # TODO: Añadir comprobación de variables existentes para evitar duplicados
+            echo "export USER=$(whoami)" >> $HOME/.zshrc
+            echo "export MAIL=$(whoami)@student.42malaga.com" >> $HOME/.zshrc
+        fi
         print_ok
     else
         # en caso positivo, establecemos el usuario al proporcionado
         print_info "Estableciendo el usuario a '${COLOR_YELLOW}$1${COLOR_WHITE}'..."
         # se añade al final del archivo las variables user42 y mail42
-        echo "let g:user42 = '$1'" >> ~/.vimrc
-        echo "let g:mail42 = '$1@student.42malaga.com'" >> ~/.vimrc
-        echo "USER=$1" >> ~/.zshrc
-        echo "MAIL=$1@student.42malaga.com" >> ~/.zshrc
+        echo "let g:user42 = '$1'" >> $HOME/.vimrc
+        echo "let g:mail42 = '$1@student.42malaga.com'" >> $HOME/.vimrc
+        echo "export USER=$1" >> $HOME/.zshrc
+        echo "export MAIL=$1@student.42malaga.com" >> $HOME/.zshrc
         print_ok
     fi
     print_info "¡Listo!. Plugin configurado correctamente.\n"
